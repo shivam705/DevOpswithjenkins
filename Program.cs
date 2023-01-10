aac9a2dbc9abe4db7dcf279f67a86cd5ffe72c4d
Shivam Kushwaha <shivam.kushwaha@nagarro.com>
2023-01-11 03:44:52 +0530

    f

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace DevOpswithjenkins
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello this is my freestyle project!");
         }
     }
 }
